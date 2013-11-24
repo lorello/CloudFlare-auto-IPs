bf5f391fe038530c7e4b5c366193e58b1f073c6e
lorello <lorenzo.salvadorini@softecspa.it>
2013-11-25 00:47:14 +0100

    Adapted to run on Ubuntu with UFW

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ CloudFlare-auto-IPs v0.2
 Automaticlly update your server's firewall to allow CloudFlare IPs
 
 Features:
-Works with CSF and IPtables
-Runs each day checking for new CF IPs to add
+- Works with Ubuntu Firewall and IPtables
+- Runs each day checking for new CloudFlare IPs to add
 
 How to install:
 
 wget https://raw.github.com/lorello/CloudFlare-auto-IPs/master/cf.sh
 sh cf.sh
 
-This will setup the script in your server for to run each day checking for new IPs to add.
+This will setup the script in your server to run each day checking for new IPs to add.
+
+Please, verify that UFW is enabled, this script doesn't activate it.
--- cf.sh
+++ cf.sh
@@ -6,88 +6,96 @@
 #
 #
 TYPE="allow"
-CRON="/etc/cron.d/cf.cron"
-LOCATION="/usr/local/cf"
-IPT_LOCATION="/sbin/iptables"
+CRON="/etc/cron.d/cloudflare-networks-refresh"
+LOCATION="/usr/local/bin/cloudflare-networks-refresh"
+IPT="/sbin/iptables"
 PORTS="80,443"
 CURRENT=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)/$(basename -- "$0")");
+UFW=`which ufw`
 
 addtocron() {
-    rm -f $CRON
-    sleep 1
-    service crond restart
-    sleep 1
-    echo "SHELL=/bin/sh" > $CRON
-    echo "0 0 * * * $LOCATION >/dev/null 2>&1" >> $CRON
-    service crond restart
+  [ -f $CRON ] && rm -f $CRON
+  echo "SHELL=/bin/sh" > $CRON
+  echo "0 0 * * * [ -x $LOCATION ] && $LOCATION >/dev/null 2>&1" >> $CRON
+  if [ -f $CRON ]; then
+    echo -e "Cron setup finished successfully:\n\t$CRON\n"
+  else
+    echo -e "ERROR: unable to create cron entry in $CRON"
+    exit 1
+  fi
 }
 
 install() {
-    if [ ! -f $LOCATION ];
-    then
-            mv $CURRENT $LOCATION
-    fi
-
-    addtocron
-    echo 'CF auto IPs has been installed'
-    echo 'This script has been created by @HKirste'
+  if [ ! -f $LOCATION ];
+  then
+    cp $CURRENT $LOCATION
+  fi
+  if [ -f $LOCATION ]; then
+    echo -e "CloudFlare ip refresher installed in\n\t$LOCATION\n"
+    return 0
+  else
+    echo -e "ERROR: unable to install command in $LOCATION"
+    exit 1
+  fi 
 }
 
 showhelp() {
-    echo 'Usage: cf.sh [OPTIONS]'
-    echo 'OPTIONS:'
-    echo '-h | --help: Show this help screen'
-    echo '-i | --install: Installs this script'
-    echo '-c | --cron: Create cron job to run this script daily'
-    echo '-t | --type: Choose between denying or allowing CF ips. Default: Allow. Valid input: ALLOW - DENY'
+  echo 'Usage: cf.sh [OPTIONS]'
+  echo 'OPTIONS:'
+  echo '-h | --help: Show this help screen'
+  echo '-i | --install: Installs this script'
+  echo '-c | --cron: Create cron job to run this script daily'
+  echo '-t | --type: Choose between denying or allowing CF ips. Default: Allow. Valid input: ALLOW - DENY'
 }
 
 while [ $1 ]; do
-    case $1 in
-        '-h' | '--help' | '?')
-            showhelp
-            exit
-        ;;
-        '-c' | '--cron' )
-            addtocron
-            exit
-        ;;
-        '--install' )
-            install
-            exit
-        ;;
-        '-t' | '--type' )
-            TYPE = $1
-            if [ "$TYPE" != "deny" -o "$TYPE" != "DENY" -o "$TYPE" != "allow" -o "$TYPE" != "ALLOW" ]; then
-                TYPE="allow"
-            fi
-        ;;
-        * )
-            showhelp
-            exit
-        ;;
-esac
-    shift
+  case $1 in
+    '-h' | '--help' | '?')
+      showhelp
+      exit
+      ;;
+    '-c' | '--cron' )
+      addtocron
+      exit $?
+      ;;
+    '--install' )
+      install
+      exit $?
+      ;;
+    '-t' | '--type' )
+      TYPE = $1
+      if [ "$TYPE" != "deny" -o "$TYPE" != "DENY" -o "$TYPE" != "allow" -o "$TYPE" != "ALLOW" ]; then
+        TYPE=allow
+      fi
+      ;;
+    * )
+      showhelp
+      exit
+      ;;
+  esac
+  shift
 done
 
 if [ ! -f $LOCATION ]; then
-    install
+  install
+  addtocron
 fi
 
-for line in `curl --silent https://www.cloudflare.com/ips-v4`; do
+for LINE in `curl --silent https://www.cloudflare.com/ips-v4`; do
 
-if [ "$TYPE" == "allow" -o "$TYPE" == "ALLOW" ]; then
+  if [ "$TYPE" == "allow" -o "$TYPE" == "ALLOW" ]; then
     if [ UFW ]; then
-        ufw allow proto tcp from $line to any port $PORTS
+      ufw allow proto tcp from $LINE to any port $PORTS
     else
-        $IPT -I INPUT -s $CURR_LINE_IP -j ALLOW
+      $IPT -I INPUT -s $LINE -j ALLOW
     fi
-elif [ "$TYPE" == "deny" -o "$TYPE" == "DENY" ]; then
+  elif [ "$TYPE" == "deny" -o "$TYPE" == "DENY" ]; then
     if [ UFW ]; then
-        ufw deny proto tcp from $line to any port 80,443
+      ufw deny proto tcp from $LINE to any port $PORTS 
     else
-        $IPT -I INPUT -s $CURR_LINE_IP -j DROP
+      $IPT -I INPUT -s $LINE -j DROP
     fi
-fi
+  fi
 
 done
+
